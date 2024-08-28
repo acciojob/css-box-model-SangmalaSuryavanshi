c6f340d909ee20c14b8c6e427f9481a3c02c14b8
SangmalaSuryavanshi <161364128+SangmalaSuryavanshi@users.noreply.github.com>
2024-08-28 21:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+cy.visit(baseUrl + "/main.html");
+cy.get('div').then(ans => {
+    let text = ans.text().trim();
+    expect(text).to.eq('This is a demo of box model, try to replicate this box model in Html & Css and pass all the test cases.');
+});
